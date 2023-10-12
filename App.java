f846bcd8806e8d468ba3a9f7c67275d3ea3e5b91
cbrown2121 <70281773+cbrown2121@users.noreply.github.com>
2023-10-12 13:04:56 -0400

    Update App.java

--- App.java
+++ App.java
@@ -1,8 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int i = 5;
-        int j = i-- +i;
-        System.out.println(j);
-        System.out.println(i);
+        System.out.println("Hello world");
     }
 }
